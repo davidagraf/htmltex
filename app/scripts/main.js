4b19f59154cac94caa8d0ad05c313b4964647e83
David Graf <davidagraf@gmail.com>
2014-02-14 07:37:02 +0100

    numbering with javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,29 @@
-/* global mathjs:true */
-
 'use strict';
 
 (function() {
-  var $toc = $('#toc');
+  var $toc = $('#toc'),
+      depths = [],
+
+  genNumbers = function(depth) {
+    var i;
+    depths.splice(depth, Infinity);
+
+    for (i = 0; i < depth; ++i) {
+      if (!depths[i]) {
+        depths[i] = 0;
+      }
+    }
+
+    ++depths[depth - 1];
+
+    return depths.join('.');
+  }
+  ;
 
   $(':header').each(function(key, value) {
-    $toc.append('<p>' + $(value).text() + '</p>');
+    var depth = +value.nodeName.substring(1),
+        newtext = genNumbers(depth) + ' ' + $(value).text();
+    $(value).text(newtext); 
+    $toc.append('<p>' + newtext + '</p>');
   });
 })();
