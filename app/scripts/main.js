a1019a7ac7c718030f279ce3d547493122a96d89
David Graf <davidagraf@gmail.com>
2014-03-11 09:04:21 +0100

    pages in the browser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,8 @@
   var $toc = $('#toc'),
       depths = [],
       headerCounter = 0,
+      A4_HEIGHT = 842, pageHeight, pageDiv,
+      DIV_ARTICLE = $('article'),
 
   genNumbers = function(depth) {
     var i;
@@ -24,8 +26,23 @@
   $(':header:not(.clean)').each(function(key, value) {
     var depth = +value.nodeName.substring(1),
         newtext = genNumbers(depth) + ' ' + $(value).text();
-    $(value).text(newtext); 
+    $(value).text(newtext);
     $(value).attr('id', 'header' + headerCounter);
-    $toc.append('<a class="tocentry" href="#header' + headerCounter++ + '">' + newtext + '</a><br/>');
+    $toc.append('<a href="#header' + headerCounter++ + '">' + newtext + '</a><br/>');
   });
+
+  pageHeight = Infinity;
+  DIV_ARTICLE.children().each(function(key, value) {
+    var $value = $(value), elHeight = $value.outerHeight();
+
+    if ($value.hasClass('page-break') || pageHeight + elHeight > A4_HEIGHT) {
+      pageHeight = 0;
+      pageDiv = $('<div class="page"></div>');
+      DIV_ARTICLE.append(pageDiv);
+    }
+    pageHeight += elHeight;
+    console.log(pageHeight);
+    pageDiv.append($value);
+  });
+
 })();
