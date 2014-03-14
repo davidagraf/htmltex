b324cad8d7e0fad1477a61e61789c468a1f7cffe
David Graf <davidagraf@gmail.com>
2014-03-14 08:35:02 +0100

    splitting paragraphs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,6 +20,23 @@
     ++depths[depth - 1];
 
     return depths.join('.');
+  },
+
+  pageFull = function() {
+    return pageDiv.prop('offsetHeight') - pageDiv.prop('scrollHeight') < 0;
+  },
+
+  appendParaPart = function(curP, part) {
+    curP.append(part);
+    if (pageFull()) {
+      curP.contents().last().remove();
+      pageDiv = $('<div class="page"></div>');
+      DIV_ARTICLE.append(pageDiv);
+      curP = $('<p/>'); 
+      pageDiv.append(curP);
+      curP.append(part);
+    }
+    return curP;
   }
   ;
 
@@ -32,18 +49,34 @@
   });
 
   DIV_ARTICLE.children().each(function(key, value) {
-    var $value = $(value), elHeight = $value.outerHeight(true);
+    var $value = $(value), elHeight = $value.outerHeight(true),
+        curP;
 
     if ($value.hasClass('page-break') || !pageDiv) {
       pageDiv = $('<div class="page"></div>');
       DIV_ARTICLE.append(pageDiv);
     }
     pageDiv.append($value);
-    if (pageDiv.prop('offsetHeight') - pageDiv.prop('scrollHeight') < 0) {
+    if (pageFull()) {
       $value.remove();
-      pageDiv = $('<div class="page"></div>');
-      DIV_ARTICLE.append(pageDiv);
-      pageDiv.append($value);
+      if (value.tagName.toLowerCase() === 'p') {
+        curP = $('<p/>');
+        pageDiv.append(curP);
+
+        $value.contents().each(function(i, v){
+          if (v instanceof Text) {
+            $.each($(v).text().trim().split(' '), function(j,w) {
+              curP = appendParaPart(curP, ' ' + w);
+            });
+          } else {
+            curP = appendParaPart(curP);
+          }
+        });
+      } else {
+        pageDiv = $('<div class="page"></div>');
+        DIV_ARTICLE.append(pageDiv);
+        pageDiv.append($value);
+      }
     }
   });
 
